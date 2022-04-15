9a1992915df0cec67d1be8c879e44dbe25f3aa37
duongcongit <duongcongit@gmail.com>
2022-04-16 00:25:39 +0700

    [CONG MINH] create interface

---
+++ virtual-keyboard/virtualkeyboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace virtual_keyboard
+{
+    public partial class virtualkeyboard : Form
+    {
+        public virtualkeyboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
