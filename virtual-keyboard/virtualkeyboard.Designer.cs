1aa368928ddb2660fd02cdb7b86f159247e4c695
duongcongit <duongcongit@gmail.com>
2022-04-16 00:30:21 +0700

    set keyboard always on top

--- virtual-keyboard/virtualkeyboard.Designer.cs
+++ virtual-keyboard/virtualkeyboard.Designer.cs
@@ -1384,7 +1384,8 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "virtualkeyboard";
-            this.Text = "Form1";
+            this.Text = "Bàn phím ảo";
+            this.Load += new System.EventHandler(this.virtualkeyboard_Load);
             this.ResumeLayout(false);
 
         }
--- virtual-keyboard/virtualkeyboard.cs
+++ virtual-keyboard/virtualkeyboard.cs
@@ -7,14 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace virtual_keyboard
 {
     public partial class virtualkeyboard : Form
     {
+
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private const UInt32 SWP_NOSIZE = 0x0001;
+        private const UInt32 SWP_NOMOVE = 0x0002;
+        private const UInt32 TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+
         public virtualkeyboard()
         {
             InitializeComponent();
+        }
+
+        private void virtualkeyboard_Load(object sender, EventArgs e)
+        {
+            SetWindowPos(this.Handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
         }
     }
 }
