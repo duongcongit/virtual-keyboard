37e56ceb11a2e358f18e1bc4cd80765c6b0c75d2
duongcongit <duongcongit@gmail.com>
2022-04-18 02:44:22 +0700

    add icon

--- virtual-keyboard/virtualkeyboard.Designer.cs
+++ virtual-keyboard/virtualkeyboard.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(virtualkeyboard));
             this.LEFT = new System.Windows.Forms.Button();
             this.DOWN = new System.Windows.Forms.Button();
             this.RIGHT = new System.Windows.Forms.Button();
@@ -2358,6 +2359,7 @@
             this.Controls.Add(this.PAUSE_BREAK);
             this.Controls.Add(this.INSERT);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "virtualkeyboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
